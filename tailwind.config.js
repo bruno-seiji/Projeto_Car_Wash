8a90cb947b8ca7166e1300daf45eaf5e4e66d03c
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2022-07-22 22:55:13 -0400

    .

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: '#0A62A9',
+          escuro: '#053155'
+        }
+      }
+    }
+  },
+  plugins: []
+}
